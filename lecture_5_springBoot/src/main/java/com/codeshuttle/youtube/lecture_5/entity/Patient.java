9c31b9c549e1762fe42c1a8f835ccfa60a25c8ce
MayankGarg4343 <mgsm4343@gmail.com>
2025-09-30 22:27:46 +0530

    In lecture_5 defined the work of the entity

--- lecture_5_springBoot/src/main/java/com/codeshuttle/youtube/lecture_5/entity/Patient.java
+++ lecture_5_springBoot/src/main/java/com/codeshuttle/youtube/lecture_5/entity/Patient.java
@@ -17,7 +17,7 @@ public class Patient {
     private long id;
 
     private String name;
-    @ToString.Exclude
+    @ToString.Exclude // this is being used to not print that statement in the console.
     private LocalDate birthDate;
     private String email;
     private String gender;
